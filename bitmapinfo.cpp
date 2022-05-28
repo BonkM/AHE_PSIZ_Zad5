0c21f980da771449d1943a91a3943a7fedc90899
BonkM <83555030+BonkM@users.noreply.github.com>
2022-05-28 06:46:21 +0200

    Add DIB header support

--- bitmapinfo.cpp
+++ bitmapinfo.cpp
@@ -130,6 +130,106 @@ struct bitmap_file_header {
   };
 };
 
+struct dib_bitmap_info_header {
+  public:
+    uint32_t struct_size;
+    int32_t pixel_width;
+    int32_t pixel_height;
+    uint16_t planes;
+    uint16_t bits_per_pixel;
+    uint32_t compression;
+    uint32_t size_in_bytes;
+    int32_t horizontal_resolution_pixels_per_meter;
+    int32_t vertical_resolution_pixels_per_meter;
+    uint32_t number_of_color_indexes;
+    uint32_t number_of_required_color_indexes;
+
+    static dib_bitmap_info_header read(std::istream& is) {
+      uint32_t struct_size = read_uint32(is);
+      switch (struct_size) {
+        case 124: // BITMAPV5HEADER
+          // fallthrough
+        case 108: // BITMAPV4HEADER
+          // fallthrough
+        case 56: // BITMAPV3INFOHEADER
+          // fallthrough
+        case 52: // BITMAPV2INFOHEADER
+          // fallthrough
+        case 40: // BITMAPINFOHEADER
+          // fallthrough
+          return read_info_header(is, struct_size);
+
+        case 64: // BITMAPCOREHEADER2
+          // fallthrough
+        case 12: // BITMAPCOREHEADER
+          return read_core_header(is, struct_size);
+
+        default:
+          throw std::runtime_error("invalid dib struct size");
+      }
+    };
+
+    std::ostream& format(std::ostream& out, std::string_view prefix, std::string_view indent) {
+      out << '{' << '\n';
+      out << prefix << indent << "\"struct_size\": " << this->struct_size << ",\n";
+      out << prefix << indent << "\"pixel_width\": " << this->pixel_width << ",\n";
+      out << prefix << indent << "\"pixel_height\": " << this->pixel_height << ",\n";
+      out << prefix << indent << "\"planes\": " << this->planes << ",\n";
+      out << prefix << indent << "\"bits_per_pixel\": " << this->bits_per_pixel << ",\n";
+      out << prefix << indent << "\"compression\": " << this->compression << ",\n";
+      out << prefix << indent << "\"size_in_bytes\": " << this->size_in_bytes << ",\n";
+      out << prefix << indent << "\"horizontal_resolution_pixels_per_meter\": " << this->horizontal_resolution_pixels_per_meter << ",\n";
+      out << prefix << indent << "\"vertical_resolution_pixels_per_meter\": " << this->vertical_resolution_pixels_per_meter << ",\n";
+      out << prefix << indent << "\"number_of_color_indexes\": " << this->number_of_color_indexes << ",\n";
+      out << prefix << indent << "\"number_of_required_color_indexes\": " << this->number_of_required_color_indexes << "\n";
+      out << prefix << '}';
+      return out;
+    };
+
+  private:
+    static dib_bitmap_info_header read_core_header(std::istream& is, uint32_t size) {
+      dib_bitmap_info_header h;
+      h.struct_size = size;
+      h.pixel_width = read_uint16(is);
+      h.pixel_height = read_uint16(is);
+      h.planes = read_uint16(is);
+      h.bits_per_pixel = read_uint16(is);
+      h.compression = 0;
+      h.size_in_bytes = 0;
+      h.horizontal_resolution_pixels_per_meter = 0;
+      h.vertical_resolution_pixels_per_meter = 0;
+      h.number_of_color_indexes = 0;
+      h.number_of_required_color_indexes = 0;
+      return h;
+    };
+
+    static dib_bitmap_info_header read_info_header(std::istream& is, uint32_t size) {
+      dib_bitmap_info_header h;
+      h.struct_size = size;
+      h.pixel_width = read_int32(is);
+      h.pixel_height = read_int32(is);
+      h.planes = read_uint16(is);
+      h.bits_per_pixel = read_uint16(is);
+      h.compression = read_uint32(is);
+      h.size_in_bytes = read_uint32(is);
+      h.horizontal_resolution_pixels_per_meter = read_int32(is);
+      h.vertical_resolution_pixels_per_meter = read_int32(is);
+      h.number_of_color_indexes = read_uint32(is);
+      h.number_of_required_color_indexes = read_uint32(is);
+
+      if (h.planes != 1) {
+        throw std::runtime_error("invalid number of planes in dib structure");
+      }
+      if (h.bits_per_pixel != 1 && h.bits_per_pixel != 4 && h.bits_per_pixel != 8 && h.bits_per_pixel != 16 && h.bits_per_pixel != 24 && h.bits_per_pixel != 32) {
+        throw std::runtime_error("invalid bits per pixel");
+      }
+      if (h.compression != 0) {
+        throw std::runtime_error("compression is unsupported");
+      }
+      return h;
+    };
+};
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     std::cerr << "Usage: bitmapinfo <bitmap path>" << std::endl;
@@ -139,5 +239,7 @@ int main(int argc, char** argv) {
   std::ifstream file(argv[1]);
   std::cout << "\"bitmap_file_header\": ";
   bitmap_file_header::read(file).format(std::cout, "", "  ") << std::endl;
+  std::cout << "\"dib_bitmap_info_header\": ";
+  dib_bitmap_info_header::read(file).format(std::cout, "", "  ") << std::endl;
   return 0;
 }
